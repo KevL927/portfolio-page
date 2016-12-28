00299e821c498fbcc1c310d70adb39bae0b857e3
KL012 <leekevin927@gmail.com>
2016-12-27 23:18:08 -0500

    Fix changing color on nav bar menu when reaching contact me section

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,19 @@
 $(() => {
-    $(window).on("load scroll", function () {
+    $(window).scroll(() => {
         let scrollPosition = $(window).scrollTop(),
             aboutPosition = $('#about-me').offset().top,
             projectsPosition = $('#projects').offset().top,
-            contactMePosition = 6643;
+            contactMePosition = 6122;
 
         if (scrollPosition >= aboutPosition && scrollPosition < projectsPosition) {
           addRemoveClass('#about-me-nav', '#intro-nav', '#projects-nav');
         } else if (scrollPosition >= projectsPosition && scrollPosition < contactMePosition) {
             addRemoveClass('#projects-nav', '#about-me-nav', '#contact-me-nav');
-        } else if (scrollPosition === contactMePosition) {
+        } else if (scrollPosition >= contactMePosition) {
             addRemoveClass('#contact-me-nav', '#projects-nav');
         } else {
             addRemoveClass('#intro-nav', '#about-me-nav');
         }
-
     });
 });
 
